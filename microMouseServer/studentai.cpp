6c19fe31d44612f3a130b52dcca776eaf596d2f7
messiscores00 <messiscores00@gmail.com>
2017-11-06 22:28:47 -0800

    stuff

--- microMouseServer/studentai.cpp
+++ microMouseServer/studentai.cpp
@@ -3,7 +3,13 @@
 #include <iostream>
 #include <string>
 using namespace std;
-#define array_length 20
+#define ARRAY_LENGTH 20
+#define LEFT 0
+#define RIGHT 2
+#define FORWARD 1
+#define BACK 3
+
+
 /*
  * The following are the eight functions that you can call. Feel free to create your own fuctions as well.
  * Remember that any solution that calls moveForward more than once per call of studentAI() will have points deducted.
@@ -25,233 +31,272 @@ using namespace std;
 
 
 //global variables
-int array1 [array_length][array_length];//a 20 by 20 array
+int array1 [ARRAY_LENGTH][ARRAY_LENGTH];//a 20 by 20 array
 int x = 0;//x position of the mouse
 int y = 0;//y position of the mouse
 bool Setarray = false;//to test to see if you have set all of the array to -1
-bool arrayset1 = false;//to see if you have set the bottom left corner to 1
-int orientation = 1;//0=face left / 1=face forwards / 2=face right / 3=face back
-int var = 0;//for "for loops"
-int var1 = 0;
+bool mouseto1 = false;//to see if you have set the bottom left corner to 1
+int orientation = FORWARD;//0=face left / 1=face forwards / 2=face right / 3=face back
+
+
+//these frunctions are used to help write less code
+int Infront (int newPos){
+    int Pos;
+    if(orientation == FORWARD){
+        if(newPos == -1)
+            Pos = array1[::x][::y+1];
+        else
+            Pos = array1[::x][::y+1] = newPos;
+    }
+    if(orientation == LEFT){
+        if(newPos == -1)
+            Pos = array1[::x-1][::y];
+        else
+            Pos = array1[::x-1][::y] = newPos;
+    }
+    if(orientation == RIGHT){
+        if(newPos == -1)
+            Pos = array1[::x+1][::y];
+        else
+            Pos = array1[::x+1][::y] = newPos;
+    }
+    if(orientation == BACK){
+        if(newPos == -1)
+            Pos = array1[::x][::y-1];
+        else
+            Pos = array1[::x+1][::y-1] = newPos;
+    }
+    return Pos;
+}
+
+
+int Right(int newPos){
+    int Pos;
+    if(orientation == FORWARD){
+        if(newPos == -1)
+            Pos = array1[::x+1][::y];
+        else
+            Pos = array1[::x+1][::y] = newPos;
+    }
+    if(orientation == LEFT){
+        if(newPos == -1)
+            Pos = array1[::x][::y+1];
+        else
+            Pos = array1[::x][::y+1] = newPos;
+    }
+    if(orientation == RIGHT){
+        if(newPos == -1)
+            Pos = array1[::x][::y-1];
+        else
+            Pos = array1[::x][::y-1] = newPos;
+    }
+    if(orientation == BACK){
+        if(newPos == -1)
+            Pos = array1[::x-1][::y];
+        else
+            Pos = array1[::x-1][::y] = newPos;
+    }
+    return Pos;
+}
+
+
+int Left(int newPos){
+    int Pos;
+    if(orientation == FORWARD){
+        if(newPos == -1)
+            Pos = array1[::x-1][::y];
+        else
+            Pos = array1[::x-1][::y] = newPos;
+    }
+    if(orientation == LEFT){
+        if(newPos == -1)
+            Pos = array1[::x][::y-1];
+        else
+            Pos = array1[::x][::y-1] = newPos;
+    }
+    if(orientation == RIGHT){
+        if(newPos == -1)
+            Pos = array1[::x][::y+1];
+        else
+            Pos = array1[::x][::y+1] = newPos;
+    }
+    if(orientation == BACK){
+        if(newPos == -1)
+            Pos = array1[::x+1][::y];
+        else
+            Pos = array1[::x+1][::y] = newPos;
+    }
+    return Pos;
+}
+
+
+int Back(int newPos){
+    int Pos;
+    if(orientation == FORWARD){
+        if(newPos == -1)
+            Pos = array1[::x][::y-1];
+        else
+            Pos = array1[::x][::y-1] = newPos;
+    }
+    if(orientation == LEFT){
+        if(newPos == -1)
+            Pos = array1[::x+1][::y];
+        else
+            Pos = array1[::x+1][::y] = newPos;
+    }
+    if(orientation == RIGHT){
+        if(newPos == -1)
+            Pos = array1[::x-1][::y];
+        else
+            Pos = array1[::x-1][::y] = newPos;
+    }
+    if(orientation == BACK){
+        if(newPos == -1)
+            Pos = array1[::x][::y+1];
+        else
+            Pos = array1[::x][::y+1] = newPos;
+    }
+    return Pos;
+}
 
 
 void microMouseServer::studentAI() {
+
+    //set all elements to the array to -1(once)
     if(::Setarray==false){//if the array is already set do nothing
-        //set array to all -1s
-        for(int i = 0; i < array_length; i += 1){
-            for(int a = 0; a < array_length; a += 1){
+        for(int i = 0; i < ARRAY_LENGTH; i += 1){
+            for(int a = 0; a < ARRAY_LENGTH; a += 1){
                 array1 [i][a]=-1;
             }
         }
         ::Setarray = true;
     }
 
-    if(::arrayset1 ==false){
+
+    //make mouse to 1(once)
+    if(mouseto1 ==false){
         array1[::x][::y] = 1;
-        ::arrayset1 = true;
+        mouseto1 = true;
     }
-    //set 0s around 1
-    //come back /change "is walls" to something that doesn't kape
-    /* if(!((::x == 0 || ::x == 19) && (::y == 0 || ::y == 19))){//if x-1 is 0-1 the array would be [-1][y] which can't happen
-            if(array1[::x-1][::y] ==1 || array1[::x+1][::y]==1){//testing if any blocks around the array are 1 then it won't set anything to 0.
-                if(!(array1[::x-1][::y] == 1)  ){//if to the left of the mouse is not 1(so -1 or 0)
-                    if(!isWallLeft()){//if there is a space left
-                        array1 [::x-1][::y] = 0;//setting the block to the left of the mouse to 0
-                    }
-                    if(!(array1[::x][::y+1] == 1) ){//if forwards is not 1(so 0 or 0)
-                        if(!isWallForward()){//if there is a speace forwards(note that this is not infront of the mouse because infront of the mouse could be to the right or left)
-                            array1 [::x][::y+1] = 0;//setting the block to forwards of the mouse to 0
-                        }
-                    }
-                    if(!(array1[::x+1][::y] == 1)){//if to the right of the mouse is not 1(so 0 and -1)
-                        if(!isWallRight()){//if there is a space to the right
-                            array1 [::x+1][::y] = 0;//setting the block to the right of the mouse to 0
-                        }
-                    }
-                }
-            }
+
+
+    //make everything around the mouse to 0 if it is -1 and if space is there
+    //infront
+    if(!isWallForward()){
+        if(Infront(-1)==-1){
+            Infront(0);
         }
-
-*/
-
-    //if mouse has already been there turn right
-    bool A = true;
-    while(A == true) {
-
-        bool C = true;
-        while(C == true){
-
-            int z = array1[::x][::y];
-
-            if(array1[::x][::y]==1||array1[::x][::y] < 1){
-                array1[::x][::y] = array1[::x][::y] + 1;
-            } else{
-                array1[::x][::y] = 1;
-            }
-
-            C = false;
-
-//check when x and y are being set cause this should work. also check orientation
-            for(int i = 1; i < z + 1 ; i++){
-//change put ::orientation
-              // printUI(std::to_string(i).c_str());
-
-                if( ((array1[::x][::y+1]==i)&&(::orientation == 1)) || ((array1[::x+1][::y]==i)&&(::orientation==2)) || ((array1[::x][::y-1]==i)&&(::orientation==3)) || ((array1[::x-1][::y]==i)&&(::orientation==0))) {//if the block infront of the mouse is 1(note that forwards is whatever the block ontop of the mouse and doesn't include its orientation)
-                    if((array1[::x+1][::y]==z-i&&::orientation==1) || (array1[::x][::y-1]==z-i&&::orientation==2) || (array1[::x-1][::y]==z-i&&::orientation==3) || (array1[::x][::y+1]==z-i&&::orientation==0) ){//if the block to the right of the mouse is not 1(this includes the orientation so if the orientation moves the block to the right of the mouse will also move)
-
-
-                        if(!isWallRight() ) {//if there is a blank space to the right
-
-                            turnRight();
-                            moveForward();
-
-                            if(::orientation == 1){
-                                ::x  = ::x + 1;
-                                ::orientation = 2;
-
-                            }
-
-                            if(::orientation == 2){
-                                ::y = ::y -1;
-                                ::orientation = 3;
-                            }
-
-                            if(::orientation == 3){
-                                ::x = ::x - 1;
-                                ::orientation = 0;
-
-                            }
-
-                            if(::orientation == 0){
-                                ::y = ::y + 1;
-                                ::orientation = 1;
-                            }
-                            A = false;
-                            C = false;
-                        }
-                    }
-                }
-            }
+    }
+    //Right
+    if(!isWallRight()){
+        if(Right(-1)==-1){
+            Right(0);
         }
-
-
-        //Left Hand rule
-        bool B= true;
-        while(B==true){
-            if(!isWallLeft()){//if space is left
-                turnLeft();
-                moveForward();
-
-                if(::orientation == 1){
-                    ::x = ::x - 1;
-                    ::orientation = 0;
-
-                }
-                if(::orientation == 2){
-                    ::y = ::y +1;
-                    ::orientation = 1;
-                }
-
-                if(::orientation == 3){
-                    ::x = ::x +1;
-                    ::orientation = 2;
-
-                }
-                if(::orientation == 0){
-                    ::y = ::y - 1;
-                    ::orientation = 3;
-                }
-
-                A = false;
-                B = false;
-            }
-            else if (!isWallForward()){//if there is a space infront
-                moveForward();
-
-
-                    if(::orientation == 1){
-                        ::y = ::y + 1;
-                        ::orientation = 1;
-                    }
-
-                    if(::orientation == 2){
-                        ::x = ::x +1;
-                        ::orientation = 2;
-
-                    }
-                    if(::orientation == 3){
-                        ::y = ::y - 1;
-                        ::orientation = 3;
-                    }
-
-                    if(::orientation == 0){
-                        ::x = ::x -1;
-                        ::orientation = 0;
-                    }
-                    A = false;
-                    B = false;
-
-            }
-            else if(!isWallRight()){//if there is a space to the right
-
-                turnRight();
-                moveForward();
-
-                if(::orientation == 1){
-                    ::x  = ::x + 1;
-                    ::orientation = 2;
-                }
-
-                if(::orientation == 2){
-                    ::y = ::y - 1;
-                    ::orientation = 3;
-                }
-
-                if(::orientation == 3){
-                    ::x = ::x - 1;
-                    ::orientation = 0;
-                }
-
-                if(::orientation == 0){
-                    ::y = ::y + 1;
-                    ::orientation = 1;
-                }
-
-                A = false;
-                B = false;
-            }
-            else if(isWallRight()){//if there is a wall to the left & infront & to the right
-                //180 turn
-                turnRight();
-                turnRight();
-                moveForward();
-                if(::orientation == 1){
-                    ::y = ::y - 1;
-                    ::orientation = 3;
-                }
-
-                if(::orientation == 2){
-                    ::x = ::x - 1;
-                    ::orientation = 0;
-
-                }
-                if(::orientation == 3){
-                    ::y = ::y + 1;
-                    ::orientation = 1;
-                }
-                if(::orientation == 0){
-                    ::x = ::x + 1;
-                    ::orientation = 2;
-                }
-
-                A = false;
-                B = false;
-            }
-            A = false;
-            B = false;
-
+    }
+    //Left
+    if(!isWallLeft()){
+        if(Left(-1)==-1){
+            Left(0);
         }
-   }
+    }
+
+
+    //Left Hand rule+right
+    if(!isWallLeft()&&((Left(-1)<=Infront(-1)||Left(-1)==0)||(Left(-1)<=Right(-1)||Left(-1)==0))){//if space is left
+        turnLeft();
+        moveForward();
+
+        if(::orientation == FORWARD){
+            ::x = ::x - 1;
+            ::orientation = LEFT;
+        }
+        else if(::orientation == RIGHT){
+            ::y = ::y +1;
+            ::orientation = FORWARD;
+        }
+        else if(::orientation == BACK){
+            ::x = ::x +1;
+            ::orientation = RIGHT;
+        }
+        else if(::orientation == LEFT){
+            ::y = ::y - 1;
+            ::orientation = BACK;
+        }
+    }
+
+
+    else if (!isWallForward()&&((Infront(-1)<=Right(-1))||(Infront(-1)==0))){//if there is a space infront
+        moveForward();
+
+        if(::orientation == FORWARD){
+            ::y = ::y + 1;
+        }
+        else  if(::orientation == RIGHT){
+            ::x = ::x +1;
+        }
+        else  if(::orientation == BACK){
+            ::y = ::y - 1;
+        }
+        else    if(::orientation == LEFT){
+            ::x = ::x -1;
+        }
+    }
+
+
+    else if(!isWallRight()){//if there is a space to the right
+        turnRight();
+        moveForward();
+
+        if(::orientation == FORWARD){
+            ::x  = ::x + 1;
+            ::orientation = RIGHT;
+        }
+        else  if(::orientation == RIGHT){
+            ::y = ::y - 1;
+            ::orientation = BACK;
+        }
+        else if(::orientation == BACK){
+            ::x = ::x - 1;
+            ::orientation = LEFT;
+        }
+        else  if(::orientation == LEFT){
+            ::y = ::y + 1;
+            ::orientation = FORWARD;
+        }
+    }
+
+
+    else if(isWallRight()){//if there is a wall to the left & infront & to the right
+        //180 turn
+        turnRight();
+        turnRight();
+        moveForward();
+
+        if(::orientation == FORWARD){
+            ::y = ::y - 1;
+            ::orientation = BACK;
+        }
+        else  if(::orientation == RIGHT){
+            ::x = ::x - 1;
+            ::orientation = LEFT;
+        }
+        else if(::orientation == BACK){
+            ::y = ::y + 1;
+            ::orientation = FORWARD;
+        }
+        else  if(::orientation == LEFT){
+            ::x = ::x + 1;
+            ::orientation = RIGHT;
+        }
+    }
+    //set where the mouse is to the position behind it, but when where the mouse is == to behind it make where the mouse is to one pluse the position behind.
+    /*if(!(array1[::x][::y] == Back(-1))){
+        array1[::x][::y]=Back(-1);
+    }
+    else
+        array1[::x][::y]= Back(-1) + 1;
+        */
 }
-//printUI(std::to_string(orientation).c_str());
 
+
+    //printUI(std::to_string().c_str());
